8fe61f6a9af5dd1ae979e039c6e6a58ddf13d3bf
Treswed <wednesdaytres58@gmail.com>
2025-10-10 21:20:09 +0300

    postgres

--- chef_site/settings.py
+++ chef_site/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'chef_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chef_db',           
+        'USER': 'chef_user',           
+        'PASSWORD': 'Piroh234',   
+        'HOST': 'localhost',           
+        'PORT': '5432',                   
     }
 }
 
